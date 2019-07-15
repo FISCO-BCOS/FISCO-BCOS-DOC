8bc49e35e793809e43b5b49ea8940724a55deaae
cyjseagull <yujiechen_hust@163.com>
2019-07-15 16:00:30 +0800

    modify readthedocs-analytics.js after change the account (#549)

--- _static/js/readthedocs-analytics.js
+++ _static/js/readthedocs-analytics.js
@@ -4,7 +4,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?285123d96b726de18ff927143f067067";
+  hm.src = "https://hm.baidu.com/hm.js?c0a496ba29af27733863d5f42967ed65";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
