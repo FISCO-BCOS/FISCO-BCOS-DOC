0938b768c6309aa6ca1d65382c3e9b88b387ef7c
cyjseagull <yujiechen_hust@163.com>
2019-07-15 12:54:57 +0800

    update conf.py to support analysis for english documentation (#546)
    * fix
    
    * update conf.py to support analysis for english version

---
+++ _static/js/en-readthedocs-analytics.js
@@ -0,0 +1,10 @@
+// For more details on analytics at Read the Docs, please see:
+// https://docs.readthedocs.io/en/latest/advertising-details.html#analytics
+// Skip analytics for users with Do Not Track enabled
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f1ff14bef27dc2d2ba0dffe47f550b47";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
