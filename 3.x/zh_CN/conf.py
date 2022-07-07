a358167956b192a0a6e848a431559029c9a73bb8
kyonRay <rui.kwok@outlook.com>
2022-07-07 12:40:52 +0800

    <doc>(conf): try to fix copybutton.

--- 3.x/zh_CN/conf.py
+++ 3.x/zh_CN/conf.py
@@ -33,6 +33,13 @@ source_parsers = {
 }
 source_suffix = ['.rst', '.md']
 
+copybutton_image_svg = """
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+   <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+   <rect x="9" y="3" width="6" height="4" rx="2" />
+</svg>
+"""
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -322,14 +329,6 @@ texinfo_documents = [
    author, 'fisco-dev', 'documents of FISCO BCOS',
    'Miscellaneous'),
 ]
-
-copybutton_image_svg = """
-<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-   <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-   <rect x="9" y="3" width="6" height="4" rx="2" />
-</svg>
-"""
 
 # Documents to append as an appendix to all manuals.
 #texinfo_appendices = []
